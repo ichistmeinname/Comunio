4538e4c5f706ce290f5d76e4f3275e7610dd3482
Sandra Dylus <ichistmeinname@web.de>
2013-02-01 10:42:57 +0100

    Refactor, reorganize and calculate diffpoints for two matchdays

--- comunio.hs
+++ comunio.hs
@@ -2,54 +2,58 @@ import Network.Browser
 import Network.HTTP
 import Text.XML.HXT.Core
 import Text.XML.HXT.XPath.Arrows
-import Text.XML.HXT.Curl
 import Data.List (delete)
-import Data.Tree.NTree.TypeDefs (NTree)
 
 import Debug.Trace (trace)
 import qualified Debug.Trace as D
 
-type Points = Int
-type URI = String
+---------- global read/wrote configs ----------
+writeOptions :: [SysConfig]
+writeOptions = [withIndent yes, withOutputEncoding utf8]
 
-comunioLoginPage :: String -> String -> URI
-comunioLoginPage username pw =
+readOptions :: [SysConfig]
+readOptions =  [withInputEncoding utf8, withParseHTML yes, withWarnings no]
+
+--------- shortcut selectors for testing ----------
+readSrc :: String -> IOXmlTree
+readSrc filePath =
+  readDocument readOptions filePath
+
+old :: IOXmlTree
+old = readSrc "old.html"
+
+curr :: IOXmlTree
+curr = readSrc "test.html"
+
+--------------------------
+---------- URLs ----------
+--------------------------
+type UserConfig = (Username, Password)
+type Username = String
+type Password = String
+
+type URL = String
+
+comunioLoginUrl :: UserConfig -> URL
+comunioLoginUrl (username, pw) =
  "http://www.comunio.de/login.phtml?login="
    ++ username ++ "&pass=" ++ pw ++ "&action=login"
 
-comunioLineup :: URI
-comunioLineup = "http://www.comunio.de/lineup.phtml"
+comunioLineupUrl :: URL
+comunioLineupUrl = "http://www.comunio.de/lineup.phtml"
 
-lineUpContent :: String -> String -> IO String
-lineUpContent username pw = do
-  (_,rsp) <- browse (
-               setAllowRedirects True -- handle HTTP redirects
-               >> (request $ getRequest (comunioLoginPage username pw)) -- login
-               >> (request $ getRequest comunioLineup)) -- redirect to lineup
-  return (rspBody rsp)
+-------------------------------------
+---------- HTML processing ----------
+-------------------------------------
 
-test :: String -> String -> FilePath -> IO ()
-test username pw dst = lineUpContent username pw >>= processHtml dst
+type XmlTreeValue a = a XmlTree String
+type ParsedXmlTree a = a XmlTree XmlTree
+type IOXmlTree = IOSArrow XmlTree XmlTree
+type HtmlBody = String
 
-processHtml htmlString dst =
-  runX (
-    parseHtml htmlString >>> extractLineUp >>> saveHtml dst)
-  >> return ()
-
-parseHtml :: String -> IOStateArrow s b XmlTree
-parseHtml htmlString = readString
-          [ withInputEncoding utf8
-          , withParseHTML yes
-          , withCurl []
-          ] htmlString
-
-extractLineUp :: IOSArrow (NTree XNode) (NTree XNode)
-extractLineUp = processChildren (processLineup `when` isElem)
-
-saveHtml :: FilePath -> IOStateArrow s XmlTree XmlTree
-saveHtml dst = writeDocument [withOutputEncoding utf8] dst
-
-processLineup :: ArrowXml a => a XmlTree XmlTree
+-- selects and reconstructs table-structure of lineup for further processing
+-- <players><player>...</player>...<player>...</player></players>
+processLineup :: ArrowXml cat => ParsedXmlTree cat
 processLineup =
  selem "players" $ [(getXPathTrees $
        "/html/body//div[@id=\"center\"]//div[@id=\"content\"]" ++
@@ -57,7 +61,10 @@ processLineup =
        "//div[@class=\"tablebox\"]//table" ++
        "//tr") >>> selem "player" [selectTableEntries]]
 
-selectTableEntries :: ArrowXml a => a XmlTree XmlTree
+-- selects player and points from the lineup html-table
+-- and constructs new XML structure
+-- (f.e. <name>Beister</name><points>22</points>)
+selectTableEntries :: ArrowXml cat => ParsedXmlTree cat
 selectTableEntries =
   selem "name" [(getChildren >>> hasName "td" >>>
     hasAttrValue "align" (== "left") >>> getChildren)]
@@ -65,51 +72,118 @@ selectTableEntries =
   selem "points" [(getChildren >>> hasName "td" >>>
     hasAttrValue "align" (== "right") >>> getChildren)]
 
-readSrc :: String -> IOStateArrow s b XmlTree
-readSrc filePath = readDocument [withTrace 0
-                , withInputEncoding utf8, withWarnings no] filePath
+extractLineUp :: IOXmlTree
+extractLineUp = processChildren (processLineup `when` isElem)
 
-computeDiff :: IOSArrow XmlTree XmlTree -> IOSArrow XmlTree XmlTree
-            -> IO [(Int, Int)]
-computeDiff newSrc oldSrc = do
-  players <- runX (newSrc >>> playerNames)
-  mapM (comparePlayers newSrc oldSrc) (map removeStars players)
+-- parses a given HtmlBody with utf8-encoding
+parseHtml :: HtmlBody -> IOStateArrow s b XmlTree
+parseHtml htmlString = readString readOptions htmlString
 
-comparePlayers :: IOSArrow XmlTree XmlTree -> IOSArrow XmlTree XmlTree
-               -> String -> IO (Points, Points)
-comparePlayers src1 src2 pName = do
-  [currPoints] <- runX (src1 >>> pointsForPlayer pName)
-  [oldPoints] <- runX (src2 >>> pointsForPlayer pName)
-  return (read currPoints :: Int, read oldPoints :: Int)
+-- saves a given XmlTree to destionation file with utf-encoding
+-- in XML representation (<xml> ... </xml>)
+saveHtml :: FilePath -> IOXmlTree
+saveHtml dst = writeDocument [withOutputEncoding utf8, withIndent yes] dst
 
-treeToText :: ArrowXml cat => cat XmlTree XmlTree -> cat XmlTree String
-treeToText selector = selector `when` isElem >>> getText
+processHtml :: HtmlBody -> FilePath -> IO ()
+processHtml htmlString dst =
+  runX (
+    parseHtml htmlString >>> extractLineUp >>> saveHtml dst)
+  >> return ()
 
-pointsForPlayer :: ArrowXml cat => String -> cat XmlTree String
-pointsForPlayer name =
-  treeToText
-   (pointsForName name <+> pointsForName ("*"++ name ++"*"))
+-- Login via GET-Request, handle redirect and redirect to lineup page manually
+lineUpContent :: UserConfig -> IO HtmlBody
+lineUpContent userConfig = do
+  (_,rsp) <- browse (
+               setAllowRedirects True -- handle HTTP redirects
+               >> (request $ getRequest (comunioLoginUrl userConfig)) -- login
+               >> (request $ getRequest comunioLineupUrl)) -- redirect to lineup
+  return (rspBody rsp)
 
-playerNames :: ArrowXml cat => cat XmlTree String
-playerNames = treeToText player
+-- all the above is triggered by this function
+downloadHtml :: UserConfig -> FilePath -> IO ()
+downloadHtml userConfig dst = lineUpContent userConfig >>= processHtml dst
 
-pointsForName :: ArrowXml a => String -> a XmlTree XmlTree
-pointsForName name =
-      (getXPathTrees (
-     "/players/player[name='"++ name
-      ++"']/points/text()"))
+--------------------------------------
+---------- Comparing Points ----------
+--------------------------------------
 
-player :: ArrowXml a => a XmlTree XmlTree
-player = getXPathTrees "/players/player/name/text()"
+type Points = Int
 
-old :: IOStateArrow s b XmlTree
-old = readSrc "old.html"
+-- In order to search for a player name in an older or newer lineup,
+-- we need both representations: BenchPlayer and Player
+type BenchPlayer = String
+type Player = String
 
-curr :: IOStateArrow s b XmlTree
-curr = readSrc "test.html"
-
-removeStars :: String -> String
+-- removes stars in player names ("*Beister*" becomes "Beister")
+removeStars :: Player -> BenchPlayer
 removeStars pName =
   case pName of
-   '*':name -> delete '*' name
+   '*':name -> (delete '*' name)
    _ -> pName
+
+-- adds stars in player names ("Beister" becomes "*Beister*")
+addStars :: BenchPlayer -> Player
+addStars pName =
+  case head pName of
+    '*' -> pName
+    _ -> "*" ++ pName ++ "*"
+
+-- neet function to select a value within a XmlTree as String (for debugging)
+textNodeToText :: ArrowXml cat => ParsedXmlTree cat -> XmlTreeValue cat
+textNodeToText selector = selector `when` isElem >>> getText
+
+selectTextNode :: ArrowXml cat => ParsedXmlTree cat -> ParsedXmlTree cat
+selectTextNode selector = selector `when` isElem
+
+-- select all player names
+-- (<players><player><name>A</name><points>2</points></player>
+--           <player><name>B</name><points>2</points></player>
+--  </players>
+--   becomes [A,B])
+player :: ArrowXml cat => ParsedXmlTree cat
+player = getXPathTrees "/players/player/name/text()"
+
+-- selector for player names (returns String)
+playerNames :: ArrowXml cat => XmlTreeValue cat
+playerNames = textNodeToText player
+
+-- select the points for a given player
+{-- (<players><player><name>pName</name><points>22</points></player></players>
+ becomes 22) --}
+pointsForName :: ArrowXml cat => String -> ParsedXmlTree cat
+pointsForName pName =
+  getXPathTrees (
+    "/players/player[name='"++ pName++"']/points/text()"
+    )
+
+-- selector for a player's points (returns String)
+pointsForPlayer :: ArrowXml cat => String -> XmlTreeValue cat
+pointsForPlayer pName =
+  textNodeToText
+   (pointsForName pName <+> pointsForName (addStars pName))
+
+-- returns points of a given player for two different data sources
+getPointsForPlayer :: IOXmlTree -> IOXmlTree -> String -> IO (Points, Points)
+getPointsForPlayer src1 src2 pName = do
+  [currPoints] <- runX (src1 >>> pointsForPlayer pName)
+  [oldPoints] <- runX (src2 >>> pointsForPlayer pName)
+  return (read currPoints :: Points, read oldPoints :: Points)
+
+computeDiff :: IOXmlTree -> IOXmlTree -> IO ()
+computeDiff newSrc oldSrc = do
+  pNames <- runX (newSrc >>> playerNames)
+  formattedPNames <- mapM (return . removeStars) pNames
+  pList <- mapM (getPointsForPlayer newSrc oldSrc) formattedPNames
+  diffList <- mapM (return . uncurry calcDiff) pList
+  runX (makeDiffHtml (zip diffList pNames))
+  return ()
+
+calcDiff :: Points -> Points -> Points
+calcDiff p1 p2 = p2 - p1
+
+makeDiffHtml :: [(Int, String)] -> IOSArrow a XmlTree
+makeDiffHtml pointsAndNames = root [] [mkelem "players" []
+  (map (\(p,n) -> selem "player" [ selem "name" [txt n]
+                                 , selem "diffPoints" [txt (show p)]])
+   pointsAndNames)]
+    >>> writeDocument writeOptions "diff-example.html"
